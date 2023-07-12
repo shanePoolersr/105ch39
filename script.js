6796519cc2b6a41d69cb21cb4215b3b02edc50b7
shanePoolersr <135184408+shanePoolersr@users.noreply.github.com>
2023-07-12 15:15:39 -0700

    updated code in register HTML

---
+++ script.js
@@ -0,0 +1,24 @@
+// Selecting by ID
+//document.getElementById("menu").style.display="none";
+//$("menu").hide();
+// console.log(jMenu);
+//Selecting by tagname
+//let a =document.getElementsByTagName("a");
+//for(let i=0;i<a.length;i++){
+//    a[i].style.display="none";
+//}
+// console.log(a);
+$("a").hide
+//selecting by classname
+// let container = document.getElement
+//$(".contenedor").hide();
+//console.log(container);
+//for(let i=0;i<container.length;i++){
+    container[i].style.display="none";
+//}
+//let img =document.querySelector("img")
+//$("img.first").hide();
+//$("img.tower").hide();
+//$("article:first p: last").hide();
+//let copy =$("article:last").clone();
+//$("main").append(copy);
